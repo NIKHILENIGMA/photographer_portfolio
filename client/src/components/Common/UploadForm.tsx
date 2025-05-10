ba198124a1d63163afeac8ab7794784e5c6a600a
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-05-10 10:40:15 +0530

    feat: enhance UploadForm with improved layout, file removal, and date selection functionality

--- client/src/components/Common/UploadForm.tsx
+++ client/src/components/Common/UploadForm.tsx
@@ -1,19 +1,28 @@
 import { FC } from "react";
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetDescription,
+  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { useUploadForm } from "@/hooks/useUploadForm";
+import { Button } from "../ui/button";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
+import { Trash2 } from "lucide-react";
+import { DatePicker } from "./DatePicker";
 
 const UploadForm: FC = () => {
   const {
     inputRef,
     preview,
     formDetails,
+    handleFileRemove,
     handleImageChange,
     handleInputChange,
     handleFormSubmit,
@@ -21,10 +30,15 @@ const UploadForm: FC = () => {
 
   return (
     <Sheet>
-      <SheetTrigger className="bg-(--secondary) p-2 rounded-lg cursor-pointer">
-        Upload Image
+      <SheetTrigger
+        asChild
+        className="bg-primary text-white py-0 px-2 h-10 rounded-md cursor-pointer"
+      >
+        <Button variant="default" className="h-10">
+          Upload Image
+        </Button>
       </SheetTrigger>
-      <SheetContent className="overflow-y-auto max-h-screen">
+      <SheetContent className="overflow-y-auto max-h-screen p-2">
         <SheetHeader>
           <SheetTitle>Upload Image</SheetTitle>
           <SheetDescription>
@@ -33,13 +47,11 @@ const UploadForm: FC = () => {
         </SheetHeader>
         <form
           onSubmit={handleFormSubmit}
-          className="flex flex-col items-center w-full max-w-2xl mx-auto bg-white rounded-xl shadow-lg p-8"
+          className="flex flex-col items-center w-full max-w-2xl mx-auto bg-white rounded-xl shadow-lg p-4"
         >
-          <div className="w-full space-y-6">
-            <label className="inline-block  transition-all cursor-pointer">
-              Choose Image
-            </label>
-            <input
+          <div className="w-full space-y-2">
+            <Label className=" cursor-pointer">Choose Image</Label>
+            <Input
               type="file"
               ref={inputRef}
               accept="image/*"
@@ -47,108 +59,106 @@ const UploadForm: FC = () => {
               onChange={handleImageChange}
               className="hidden"
             />
-            <div
-              onClick={() => inputRef.current?.click()}
-              className="w-full h-80 bg-gray-50 rounded-xl mb-6 flex items-center justify-center border-2 border-dashed border-gray-300 hover:border-blue-500 transition-colors"
-            >
-              {preview ? (
+            <span className="text-gray-500 text-xs px-1">
+              Click to upload an image
+            </span>
+            {preview ? (
+              <div className="relative w-full min-h-64 bg-gray-50 rounded-xl mb-6 flex items-center justify-center ">
+                <span
+                  className="absolute top-4 right-4 text-gray-500 font-medium cursor-pointer hover:text-red-500 transition-colors"
+                  onClick={handleFileRemove}
+                >
+                  <Trash2 color="red" size={14} />
+                </span>
                 <img
                   src={preview}
                   alt="Preview"
-                  className="w-full h-80 object-cover rounded-xl shadow-lg"
+                  className="w-full h-80 object-cover rounded-xl shadow-sm"
                 />
-              ) : (
-                <span className="text-gray-500 font-medium flex items-center justify-center cursor-pointer hover:text-blue-500 transition-colors">
+              </div>
+            ) : (
+              <div
+                onClick={() => inputRef.current?.click()}
+                className="w-full h-64 bg-gray-50 rounded-xl mb-6 flex items-center justify-center border-2 border-dashed border-gray-300 hover:border-primary transition-colors"
+              >
+                <span className="text-gray-500 font-medium flex items-center justify-center cursor-pointer hover:text-primary transition-colors">
                   No image selected
                 </span>
-              )}
-            </div>
+              </div>
+            )}
+
             <div className="space-y-4">
-              <div className="flex flex-col">
-                <label
-                  htmlFor="title"
-                  className="text-gray-700 font-medium mb-2"
-                >
+              <div className="flex flex-col space-y-2">
+                <Label htmlFor="title" className=" font-medium mb-2">
                   Title
-                </label>
-                <input
+                </Label>
+                <Input
                   type="text"
                   id="title"
                   name="title"
                   value={formDetails?.title || ""}
                   placeholder="Enter image title"
                   onChange={handleInputChange}
-                  className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"
+                  className="w-full "
                   required
                 />
+
+                <span className="text-gray-500 text-xs px-1">
+                  Enter a title for the image. This will be displayed as the
+                  main heading.
+                </span>
               </div>
-              <div className="flex flex-col">
-                <label
-                  htmlFor="location"
-                  className="text-gray-700 font-medium mb-2"
-                >
-                  Location
-                </label>
-                <input
+              <div className="flex flex-col space-y-2">
+                <Label htmlFor="location">Location</Label>
+                <Input
                   type="text"
                   id="location"
                   name="location"
                   value={formDetails?.location || ""}
                   onChange={handleInputChange}
                   placeholder="Enter location"
-                  className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"
+                  className="w-full"
                   required
                 />
+                <span className="text-gray-500 text-xs px-1">
+                  Enter the location where the image was taken.
+                </span>
               </div>
-              <div className="flex flex-col">
-                <label
-                  htmlFor="date"
-                  className="text-gray-700 font-medium mb-2"
-                >
-                  Date Taken
-                </label>
-                <input
-                  id="date"
-                  type="date"
-                  name="date"
-                  value={
-                    formDetails?.date
-                      ? formDetails.date.toISOString().split("T")[0]
-                      : ""
-                  }
-                  onChange={handleInputChange}
-                  className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"
-                  required
-                />
+              <div className="flex flex-col space-y-2">
+                <Label htmlFor="description" className=" font-medium mb-2">
+                  Date
+                </Label>
+                <DatePicker />
+                <span className="text-gray-500 text-xs px-1">
+                  Select the date when the image was taken.
+                </span>
               </div>
-              <div className="flex flex-col">
-                <label
-                  htmlFor="description"
-                  className="text-gray-700 font-medium mb-2"
-                >
-                  Description
-                </label>
-                <textarea
-                  name="description"
-                  id="description"
-                  rows={6}
-                  value={formDetails?.description || ""}
-                  placeholder="Enter image description"
-                  onChange={handleInputChange}
-                  className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all resize-none"
-                  required
-                  minLength={10}
+
+              <div className="grid w-full gap-1.5">
+                <Label htmlFor="message-2">Your Message</Label>
+                <Textarea
+                  placeholder="Type your message here."
+                  id="message-2"
                 />
+                <p className="text-xs text-muted-foreground">
+                  Write a brief description of the image.
+                </p>
               </div>
             </div>
           </div>
 
-          <button
-            type="submit"
-            className="mt-8 w-full max-w-xs px-6 py-3 bg-blue-500 text-white font-medium rounded-lg shadow-md hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 transition-all"
-          >
-            Upload Image
-          </button>
+          <SheetFooter className="flex w-full mt-6">
+            <Button
+              type="submit"
+              className="cursor-pointer"
+              variant={"default"}
+            >
+              Upload
+            </Button>
+            <SheetClose asChild>
+              <Button variant={'outline'} type="submit">Cancel</Button>
+            </SheetClose>
+          </SheetFooter>
         </form>
       </SheetContent>
     </Sheet>
