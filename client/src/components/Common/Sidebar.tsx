2521c50816d89bf5d3efd724ca030027647b91c6
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-05-10 10:40:08 +0530

    feat: enhance Sidebar component with improved layout and logout functionality

--- client/src/components/Common/Sidebar.tsx
+++ client/src/components/Common/Sidebar.tsx
@@ -3,31 +3,54 @@ import {
   FaTachometerAlt,
   FaAddressBook,
   FaFileAlt,
-  FaBars,
+  FaRegCircle,
+  FaCircle,
 } from "react-icons/fa";
 import { useState } from "react";
-import { NavLink } from "react-router";
+import { NavLink, useNavigate } from "react-router";
+import { Button } from "../ui/button";
+import AuthService from "@/services/api/authServices";
 
 const Sidebar: FC = () => {
   const [collapsed, setCollapsed] = useState<boolean>(false);
+  const navigate = useNavigate();
 
   const menuItems = [
     { name: "Dashboard", icon: <FaTachometerAlt />, path: "dashboard" },
     { name: "Contacts", icon: <FaAddressBook />, path: "contact-management" },
     { name: "Posts", icon: <FaFileAlt />, path: "photo-management" },
   ];
+
+  const handleLogout = async () => {
+    // Handle logout logic here
+    console.log("Logout clicked");
+    await AuthService.logout();
+    navigate("/login");
+  };
+
   return (
-    <div
-      className={`bg-sidebar border-[1px] border-r border-card text-secondary-foreground/70 transition-all duration-300 ${
-        collapsed ? "w-16" : "w-80"
-      } flex flex-col`}
+    <aside
+      className={`sticky top-0 left-0 min-h-screen bg-muted border-[1px] border-r border-border text-muted-foreground transition-all duration-300 z-50 ${
+        collapsed ? "w-16" : "w-64"
+      } flex flex-col font-MonaSans`}
     >
-      <div className="flex items-center justify-between px-4 py-4 border-b border-accent">
-        <div className={`text-lg text-black font-bold ${collapsed ? "hidden" : "block"}`}>
+      <div className="flex items-center justify-between px-4 py-4 border-b border-border">
+        <div
+          className={`text-lg text-primary font-bold ${
+            collapsed ? "hidden" : "block"
+          }`}
+        >
           MyCompany
         </div>
-        <button onClick={() => setCollapsed(!collapsed)} >
-          <FaBars />
+        <button
+          onClick={() => setCollapsed(!collapsed)}
+          className="text-muted-foreground hover:text-primary transition-colors"
+        >
+          {collapsed ? (
+            <FaRegCircle color="#f97316" />
+          ) : (
+            <FaCircle color="#f97316" />
+          )}
         </button>
       </div>
       <nav className="flex-1 mt-4">
@@ -36,19 +59,26 @@ const Sidebar: FC = () => {
             key={item.name}
             to={item.path}
             className={({ isActive }) =>
-              `flex items-center gap-4 px-4 py-3 hover:bg-sidebar-accent/50 ${
-                isActive ? "bg-sidebar-accent text-sidebar-accent-foreground" : ""
+              `flex items-center gap-4 px-4 py-3 rounded-md hover:bg-accent hover:text-accent-foreground transition-colors ${
+                isActive ? "bg-accent text-primary" : ""
               }`
             }
           >
-            <span>{item.icon}</span>
-            <span className={`${collapsed ? "hidden" : "block"}`}>
+            <span className="">{item.icon}</span>
+            <span className={`${collapsed ? "hidden" : "block"} `}>
               {item.name}
             </span>
           </NavLink>
         ))}
       </nav>
-    </div>
+      <Button
+        variant={"outline"}
+        className="mx-4 mb-4 mt-auto"
+        onClick={handleLogout}
+      >
+        Logout
+      </Button>
+    </aside>
   );
 };
 
