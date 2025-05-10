c268530950ac26896588b9c3f32e2330ef561b41
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-05-10 10:42:03 +0530

    fix: update background color and add overflow handling in AdminLayout

--- client/src/layout/AdminLayout.tsx
+++ client/src/layout/AdminLayout.tsx
@@ -5,7 +5,7 @@ export default function AdminLayout() {
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <main className="flex-1 bg-gray-100 p-6">
+      <main className="flex-1 bg-background p-6 overflow-y-auto">
         <Outlet />
       </main>
     </div>
