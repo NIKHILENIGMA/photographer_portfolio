cad0de686ca40da353e30b499ad7389c7f846707
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-05-19 00:29:42 +0530

    refactor: convert ApiResponse class to a function for standardized API responses

--- server/src/util/ApiResponse.ts
+++ server/src/util/ApiResponse.ts
@@ -1,23 +1,48 @@
-class ApiResponse<T> {
-    public success: boolean;
-    public message: string;
-    public data: T | null;
-    public statusCode: number;
+import { Request, Response } from 'express'
+import { EApplicationEnvironment } from '../types/base.types'
+import { NODE_ENV } from '../config/app.config'
 
-    constructor(success: boolean, message: string, data: T | null = null, statusCode: number = 200) {
-        this.success = success;
-        this.message = message;
-        this.data = data;
-        this.statusCode = statusCode;
+type THttpResponse = {
+    success: boolean
+    statusCode: number
+    request: {
+        ip?: string | null
+        method: string
+        url: string
     }
-
-    static successResponse<T>(message: string, data: T, statusCode: number = 200): ApiResponse<T> {
-        return new ApiResponse<T>(true, message, data, statusCode);
-    }
-
-    static errorResponse(message: string, statusCode: number = 400): ApiResponse<null> {
-        return new ApiResponse<null>(false, message, null, statusCode);
-    }
+    message: string
+    data?: unknown
 }
 
-export default ApiResponse;
+/**
+ * Sends a standardized API response.
+ *
+ * @param req - The HTTP request object.
+ * @param res - The HTTP response object.
+ * @param responseStatusCode - The status code to send in the response.
+ * @param responseMessage - The message to send in the response.
+ * @param data - Optional data to include in the response. Defaults to null.
+ *
+ * @returns void
+ */
+
+export const ApiResponse = (req: Request, res: Response, responseStatusCode: number, responseMessage: string, data: unknown = null): void => {
+    const response: THttpResponse = {
+        success: true,
+        statusCode: responseStatusCode,
+        request: {
+            ip: req.ip || null,
+            method: req.method,
+            url: req.url
+        },
+        message: responseMessage,
+        data: data
+    }
+
+    // If the environment is production, remove the IP address from the response
+    if (NODE_ENV === EApplicationEnvironment.PRODUCTION) {
+        delete response.request.ip
+    }
+
+    res.status(responseStatusCode).json(response)
+}
