bb0d3e826e939ee1ff1e29a2ebcad3fe26b0eba6
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-05-20 08:46:26 +0530

    feat: update database configuration and Prisma client initialization

--- server/src/config/app.config.ts
+++ server/src/config/app.config.ts
@@ -8,6 +8,9 @@ export const PORT = process.env.PORT || 3000;
 
 // Environment
 export const NODE_ENV = process.env.NODE_ENV || "development";
+export const DATABASE_URL = process.env.DATABASE_URL || "";
+export const isProduction = NODE_ENV === "production";
+export const isDevelopment = NODE_ENV === "development";
 
 // JWT
 export const ACCESS_TOKEN_SECRET =
--- server/src/config/prisma.config.ts
+++ server/src/config/prisma.config.ts
@@ -1,5 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../generated/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { DATABASE_URL } from "./app.config";
 
-const prisma = new PrismaClient();
+const connectionString: string = DATABASE_URL
+const adapter = new PrismaPg({
+    connectionString
+})
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
