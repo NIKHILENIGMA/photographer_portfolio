422dbf1f5e122f2695390700f7875397d93f03fe
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-05-20 08:46:56 +0530

    feat: export additional middleware for error handling and authentication

--- server/src/middleware/index.ts
+++ server/src/middleware/index.ts
@@ -1,3 +1,7 @@
 import { upload } from "./multer.middleware";
+import { notFound } from "./notFound.middleware";
+import { errorHandler } from "./errorHandler.middleware";
+import { isAuthenticated } from "./isAuthenticated.middleware";
+import * as multer from "./multer.middleware";
 
-export { upload };
+export { upload, notFound, errorHandler, isAuthenticated, multer };
